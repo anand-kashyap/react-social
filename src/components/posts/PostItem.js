f7682577ea940dfc1875af29ecf5dddfcf5ab86b
Anand Kashyap <ak@cloudify.biz>
2020-02-20 18:04:19 +0530

    add types and icons

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -2,17 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 
+import { cardTitle, date, actions } from './PostItem.module.scss';
 const PostItem = ({ data: { title, text, id, hasImage, imageUrl, createdBy, createdAt } }) => {
   return (
     // <div className="col s12">
-    <div className="card">
+    <div className="card hoverable">
       <div className="card-content">
-        <Link to={`/post/${id}`} className="card-title">{title}</Link>
+        <div className={cardTitle}>
+          <i className="material-icons-outlined">account_circle</i>
+          <Link to={`/user/${createdBy}`}>{createdBy}</Link>
+          <p className={date}>{createdAt}</p>
+        </div>
         <p>{text}</p>
       </div>
-      <div className="card-action">
-        <Link to="/">More</Link>
-        <Link to="/">Share</Link>
+      <div className={actions + " card-action"}>
+        <button className="waves-effect btn-flat">Like</button>
+        <Link className="waves-effect btn-flat" to={`/post/${id}`}>Comment</Link>
+        {/* <Link to="/">Share</Link> */}
       </div>
     </div>
     // </div>
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -5,12 +5,12 @@ import posts from '../../mockData/posts';
 const Posts = props => {
   return (
     <Fragment>
+      <div className="fixed-action-btn">
+        <button className="btn-floating btn-large red waves-effect waves-circle waves-light">
+          <i className="large material-icons-outlined">add</i>
+        </button>
+      </div>
       <div className="row">
-        <div className="fixed-action-btn">
-          <button className="btn-floating btn-large red waves-effect waves-circle waves-light">
-            <i className="large material-icons">add</i>
-          </button>
-        </div>
         {posts.map(post => (
           <PostItem key={post.id} data={post} />
         ))}
