9111c19e7559296309c4385b49d9d35214282dc0
Anand Kashyap <ak@cloudify.biz>
2020-02-20 16:15:32 +0530

    add scss and css module support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, Fragment } from 'react';
-import './App.css';
+import './App.scss';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import Posts from './components/posts/Posts';
 import Login from './components/pages/Login';
-import Header from './components/layout/Header';
+import Header from './components/layout/Header/Header';
 import Post from './components/posts/Post';
 
 function App() {
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+
+import { navWrapper, heading } from './Header.module.scss';
 const Header = () => {
   return (
     <nav>
-      <div className="nav-wrapper">
-        <Link to="/" className="heading">SnappyLog</Link>
+      <div className={navWrapper}>
+        <Link to="/" className={heading}>SnappyLog</Link>
         <ul id="nav-mobile" className="right">
           <li>
             <Link to="/login" >Login</Link>
