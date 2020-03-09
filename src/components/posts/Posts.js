0e44e7797d47878162b2de359d7509c8c6161a16
Anand Kashyap <ak@cloudify.biz>
2020-03-09 12:40:34 +0530

    modal design complete

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,14 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import PostItem from './PostItem'
 import posts from '../../mockData/posts';
 import Modal from '../utils/Modal';
 
 const Posts = props => {
+  const modalId = 'modal1';
+  const [scale, setScale] = useState('');
+  useEffect(() => {
+    setTimeout(() => {
+      setScale('scale-in');
+    }, 350);
+  }, []);
+
   return (
     <Fragment>
-      <Modal bottom={true} />
+      <Modal bottom={true} id={modalId} />
       <div className="fixed-action-btn">
-        <button data-target="modal1" className="btn-floating btn-large red waves-effect waves-circle waves-light modal-trigger">
+        <button data-target={modalId} className={`btn-floating btn-large red waves-effect waves-circle waves-light modal-trigger scale-transition scale-out ${scale}`}>
           <i className="large material-icons-outlined">add</i></button>
       </div>
       <div className="row">
--- src/components/utils/Modal.js
+++ src/components/utils/Modal.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef } from 'react';
 import { M } from './Shared';
-
+import './Modal.scss';
 const Modal = ({ bottom = false, dismiss = true, options = null, id = 'modal1' }) => {
   let modalRef = useRef();
   const mainClass = bottom ? 'bottom-sheet' : null;
   if (!options) {
     options = {
       inDuration: 250,
-      outDuration: 250,
+      outDuration: 350,
       opacity: 0.5,
       startingTop: "4%",
       endingTop: "10%"
@@ -20,26 +20,24 @@ const Modal = ({ bottom = false, dismiss = true, options = null, id = 'modal1' }
   }, []);
 
   return (
-    <div>
-      <div
-        ref={Modal => {
-          modalRef = Modal;
-        }}
-        id={id}
-        className={'modal ' + mainClass}
-      >
-        <div className="modal-content">
-          <h4>Modal Header</h4>
-          <p>A bunch of text</p>
-        </div>
-        <div className="modal-footer">
-          <p className="modal-close waves-effect waves-red btn-flat">
-            Disagree
-            </p>
-          <p className="modal-close waves-effect waves-green btn-flat">
-            Agree
-            </p>
-        </div>
+    <div
+      ref={Modal => {
+        modalRef = Modal;
+      }}
+      id={id}
+      className={'modal ' + mainClass}
+    >
+      <div className="modal-content">
+        <h5>Create Post</h5>
+        <textarea placeholder="Share something..."></textarea>
+      </div>
+      <div className="modal-footer">
+        <p className="modal-close waves-effect waves-red btn-flat">
+          Cancel
+        </p>
+        <p className="waves-effect waves-light btn red">
+          Create
+        </p>
       </div>
     </div>
   );
