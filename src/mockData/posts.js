fde441e6ec7ef22cc23459795d734b47c25bcfb3
Anand Kashyap <anandkashyap60@gmail.com>
2020-03-09 14:21:31 +0530

    setup netlify

---
+++ src/mockData/posts.js
@@ -0,0 +1,40 @@
+const posts = [
+  {
+    "id": 1,
+    "title": "Test Post 1",
+    "text": "lorem ipsum one two",
+    "hasImage": false,
+    "imageUrl": "",
+    "createdBy": "anand_ak",
+    "createdAt": "2020-02-17"
+  },
+  {
+    "id": 2,
+    "title": "Test Post 2",
+    "text": "lorem ipsum one two three",
+    "imageUrl": "",
+    "hasImage": false,
+    "createdBy": "arun",
+    "createdAt": "2020-02-16"
+  },
+  {
+    "id": 3,
+    "title": "Test Post 3",
+    "text": "lorem ipsum one four",
+    "imageUrl": "",
+    "hasImage": false,
+    "createdBy": "tarun",
+    "createdAt": "2020-02-15"
+  },
+  {
+    "id": 4,
+    "imageUrl": "",
+    "title": "Test Post 4",
+    "text": "lorem ipsum one six",
+    "hasImage": false,
+    "createdBy": "steve",
+    "createdAt": "2020-02-14"
+  }
+]
+
+export default posts;
---
+++ src/mockData/user.js
@@ -0,0 +1,4 @@
+export default user = {
+  "id": "127437",
+  "username": "anand_ak"
+}
