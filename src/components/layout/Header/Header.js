577e8ace15d58df03956beb2ae81db0f5f032508
Anand Kashyap <anandkashyap60@gmail.com>
2020-03-31 12:33:43 +0530

    final fix

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
 
   useEffect(() => {
     if (darkMode) {
-      document.body.setAttribute('data-theme', 'dark')
+      document.body.setAttribute('data-theme', 'dark');
     } else {
       document.body.removeAttribute('data-theme');
     }
