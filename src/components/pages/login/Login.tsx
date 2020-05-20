9919a53804b48c1e161b266bf9c0d92369bd25d3
Anand Kashyap <anandkashyap60@gmail.com>
2020-05-20 18:49:12 +0530

    add  escaped dollar env

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { string, object } from 'yup';
 
@@ -8,6 +8,9 @@ import axios from 'utils/api';
 
 const Login = () => {
   const history = useHistory();
+  useEffect(() => {
+    console.log(process.env.NODE_ENV);
+  }, []);
   const validateFields = () =>
     object().shape({
       email: string().required().email(),
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { verify } from 'jsonwebtoken';
 import { history } from 'components/App';
 
-const salt = '$2b$10$HwNikNajaE.2Hwf47wJ1i.',
+const salt = process.env.REACT_APP_SALT,
   ls = localStorage;
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' && !process.env.AWS ? 'https://snappyapi.herokuapp.com' : 'http://localhost:2000';
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'https://jsclub.dev/api' : 'http://localhost:2000/api';
 
 // * adding interceptors
 //* for adding token
@@ -26,8 +26,8 @@ axios.interceptors.response.use(
   (res) => {
     const { data: { token } } = res;
     if (token) { // if login
-      console.log('from api', token);
-      const payload = verify(token, salt);
+      // console.log('from api', salt);
+      const payload = verify(token, salt as string);
       ls.setItem('snappyToken', token);
       ls.setItem('snappyUser', JSON.stringify(payload));
     }
